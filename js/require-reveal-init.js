b6793ea73085bacac87fe38bcce3cce7745a5f5f
Ger Hobbelt <ger@hobbelt.com>
2014-04-06 00:08:19 +0200

    the parallax image configured in the JS config object in the index (demo) page overrides any CSS-defined parallax image, if one is available: by removing the JS-config-specified one, the parallax demo theme finally works again.

--- js/require-reveal-init.js
+++ js/require-reveal-init.js
@@ -33,8 +33,8 @@ require(['head', 'zoom', 'highlight', 'marked', 'classList', 'verge', 'reveal'],
         transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
 
         // Parallax scrolling
-        parallaxBackgroundImage: 'assets/reveal-parallax-1.jpg', // 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
-        //parallaxBackgroundSize: '2100px 900px',
+        //parallaxBackgroundImage: 'assets/reveal-parallax-1.jpg', // 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+        //parallaxBackgroundSize: '2100px 900px', [parallaxBackgroundSize is OBSOLETED]
 
         // Optional libraries used to extend on reveal.js
         dependencies: [
